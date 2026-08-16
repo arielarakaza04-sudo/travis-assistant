b410d1b6c3d8b83cec96a48d517ac401bcd832ed
arielarakaza04-sudo <arielarakaza04@gmail.com>
2026-08-16 04:42:56 +0200

    Update TaskHandler.kt

--- app/src/main/java/com/ariel/TaskHandler.kt
+++ app/src/main/java/com/ariel/TaskHandler.kt
@@ -5,6 +5,7 @@ import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.net.Uri
+import android.os.Build
 import android.os.Environment
 import android.provider.AlarmClock
 import android.provider.CalendarContract
